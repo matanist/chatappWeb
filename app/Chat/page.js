4ea0739fe417db6d34c1560a9763daeaa498921b
Fatih Baytar <matanist@hotmail.com>
2024-06-21 15:17:13 +0100

    NextAuth entegrasyon tamamlandı.

--- app/Chat/page.js
+++ app/Chat/page.js
@@ -1,6 +1,20 @@
+"use client"
 
+import { getSession } from "next-auth/react"
+import { useEffect } from "react"
 
 export default function Page() {
+  const session = getSession();
+  useEffect(()=>{
+    session.then(s=>{
+      if(s){
+        console.log(s)
+      }
+      else{
+        location.href="/";
+      }
+    });
+  },[])
   return (
     <div>Chat Sayfası</div>
   )
--- app/page.js
+++ app/page.js
@@ -1,70 +1,99 @@
 "use client";
 import Grid from "@mui/material/Unstable_Grid2";
-import { Button, TextField } from "@mui/material";
-import { useState } from "react";
+import { Button, TextField,Alert } from "@mui/material";
+import { useEffect, useState } from "react";
 import { loginRequest } from "@/requests/loginRequest";
+import {getSession, signIn} from "next-auth/react";
 
 export default function Home() {
   const [user, setUser] = useState({});
+  const [error, setError] = useState("");
+  const session = getSession();
+  useEffect(()=>{
+    session.then(s=>{
+      if(s){
+        location.href="/Chat";
+      }
+    })
+  },[]);
+  function handleSubmit(e){
+    e.preventDefault();
+    
+    const username = e.currentTarget.username.value;
+    const password = e.currentTarget.password.value;
 
-  function loginButtonClicked(){
-    console.log("user:",user);
-    loginRequest(user).then((response)=>{
-      if(response.data.success){
-        localStorage.setItem("token",response.data.data);
+    signIn("credentials",{
+      username, password, redirect:false
+    }).then(res=>{
+      if(res.ok){
+        location.href="/Chat";
+      }else if(res.error){
+        console.log(res.error);
+        setError(res.error);
       }
-      else{
-        console.log("Login failed")
-      }
-    }).catch((error)=>{
-      console.log("error:",error);
     })
   }
   return (
-    <Grid
-      height="100vh"
-      direction="column"
-      container
-      justifyContent="center"
-      alignItems="center"
-      rowGap={2}
-    >
-      <Grid xs={3} height={"30vh"} style={{
-        textAlign:'center',
-        backgroundImage:'url("images/loginBackground.jpeg")',
-        backgroundSize:'cover',
-        backgroundPosition:'center',
-        backgroundRepeat:'no-repeat',
-        borderRadius:'10px'
-      }}>
+    <form onSubmit={handleSubmit}>
+      <Grid
+        height="100vh"
+        direction="column"
+        container
+        justifyContent="center"
+        alignItems="center"
+        rowGap={2}
+      >
+        <Grid
+          xs={3}
+          height={"30vh"}
+          style={{
+            textAlign: "center",
+            backgroundImage: 'url("images/loginBackground.jpeg")',
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            borderRadius: "10px",
+          }}
+        ></Grid>
+        <Grid xs={3}>
+          <h1>Chat App Login Page</h1>
+        </Grid>
+        <Grid xs={3}>
+          <TextField
+            fullWidth
+            id="outlined-basic"
+            label="Username"
+            variant="outlined"
+            name="username"
+          />
+        </Grid>
+        <Grid xs={3}>
+          <TextField
+            fullWidth
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            type="password"
+            name="password"
+          />
+        </Grid>
+        {error &&<Grid xs={3}>
+        <Alert severity="error">{error}</Alert>
+        </Grid>}
         
+        <Grid xs={3}>
+          <Button
+            variant="contained"
+            color="primary"
+            label="Log In"
+            fullWidth
+            type="submit"
+          >
+            Log In
+          </Button>
+          <a href="/register">Register</a>
+        </Grid>
       </Grid>
-      <Grid xs={3}>
-        <h1>Chat App Login Page</h1>
-      </Grid>
-      <Grid xs={3}>
-        <TextField
-          fullWidth
-          id="outlined-basic"
-          label="Username"
-          variant="outlined"
-          onChange={(e)=>setUser({...user,username:e.target.value})}
-        />
-      </Grid>
-      <Grid xs={3}>
-        <TextField
-          fullWidth
-          id="outlined-basic"
-          label="Password"
-          variant="outlined"
-          type="password"
-          onChange={(e)=>setUser({...user,password:e.target.value})}
-        />
-      </Grid>
-      <Grid xs={3}>
-        <Button variant="contained" color='primary' label='Log In' fullWidth onClick={loginButtonClicked}>Log In</Button>
-        <a href="/register">Register</a>
-      </Grid>
-    </Grid>
+    </form>
   );
 }
